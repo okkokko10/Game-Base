2d4c6117093409bcf8f19ce089fbd9a1ecad6c82
okkokko10 <okko.heinio@gmail.com>
2021-06-08 22:41:58 +0300

    update

--- Scanner.py
+++ Scanner.py
@@ -17,11 +17,29 @@ class Ray:
         x = mul *(vy*difference.y+vx*difference.x)
         y= x*vy/vx
         return x,y
-    def IntersectCircleRelative(self,difference,radius):
+    def IntersectCircleRelative(self,difference,radius,behind=False):
         #y=\frac{v.y}{v.x}x
             #\frac{x=c.x+c.y\frac{v.y}{v.x}\pm \sqrt{\left(c.x+c.y\frac{v.y}{v.x}\right)^2-\left(\left(\frac{v.y}{v.x}\right)^2+1\right)\cdot \left(-R^2+c.y^2+c.x^2\right)}}{\left(\frac{v.y}{v.x}\right)^2+1}
         vx,vy=self.vector
         cx,cy=difference
+        if vx == 0:
+            D=radius**2-cx**2
+            if D>=0:
+                sqrtD=D**0.5
+                y1=cy+sqrtD
+                y2=cy-sqrtD
+                out=[]
+                if y1*vy>=0 or behind:
+                    out.append(V(0,y1))
+                if y2*vy>=0 or behind:
+                    out.append(V(0,y2))
+                if vy<0:
+                    return out
+                else:
+                    return reversed(out)
+            else:
+                return None
+                
         k = vy/vx
         a=k**2 +1
         b=cx+cy*k
@@ -34,11 +52,23 @@ class Ray:
             x2 = (b - sqrtD)/a
             y1 = k*x1
             y2 = k*x2
-            return (x1,y1),(x2,y2)
+            out=[]
+            if x1*vx>=0 or behind:
+                out.append(V(x1,y1))
+            if x2*vx>=0 or behind:
+                out.append(V(x2,y2))
+            if vx>0:
+                return out
+            else:
+                return reversed(out)
         else:
             return None
-    def IntersectCircle(self,center,radius):
-        return self.IntersectCircleRelative(center-self.origin,radius)+self.origin
+    def IntersectCircle(self,center,radius,behind=False):
+        out = self.IntersectCircleRelative(center-self.origin,radius,behind)
+        if out:
+            return map(lambda x: x+self.origin,out)
+        else:
+            return None
 
 
 class Obstacle(GameObject):
@@ -59,24 +89,47 @@ class Turtle(GameObject):
         self.direction=direction
         self.color=(0,100,100)
         self.radius=1/4
+        self.hits=[]
     def Draw(self,canvas:ScaledCanvas):
         canvas.DrawCircle(self.pos,self.color,self.radius)
-        canvas.DrawLine(self.pos,self.pos+self.direction,self.color,self.radius)
+        canvas.DrawLine(self.pos,self.pos+self.direction,self.color,self.radius/2)
+        for h in self.hits:
+            canvas.DrawCircle(h,self.color,1/16)
     def Update(self, scene:Scene):
-        
         if pygame.K_d in scene.keysDown:
-            self.pos+=V(1,0)
+            self.pos+=(scene.mousePos-self.pos).normalize()*scene.deltaTime/1000
+        if pygame.K_a in scene.keysDown:
+            self.direction+=(scene.mousePos-self.pos-self.direction)*scene.deltaTime/500
+        if pygame.K_s in scene.keysDown:
+            possibleHits=[]
+            for o in scene.objects:
+                if isinstance(o,Obstacle):
+                    hit=o.IntersectRay(Ray(self.pos,self.direction))
+                    if hit:
+                        possibleHits.extend(hit)
+            if possibleHits:
+                m=possibleHits[0]
+                for h in possibleHits:
+                    if self.direction.x==0:
+                        if (m.y<h.y) == (self.direction.y<0):
+                            m=h
+                    else:
+                        if (m.x<h.x) == (self.direction.x<0):
+                            m=h
+                self.hits.append(m)
+        if pygame.K_w in scene.keysDown:
+            self.hits.clear()
 
 a=Scene()
 b=Display(V(900,800))
 #c=Obstacle(pos=V(1,-2),radius=1/2,color=(100,0,50))
 #a.AddObject(c)
-a.AddObject(Turtle(V(-1,-2),V(0,1)))
-for i in range(8):
+for i in range(16):
     o=Obstacle(
         V(i/4-1,0),
         1/8
     )
     a.AddObject(o)
+a.AddObject(Turtle(V(-1,-2),V(0,1)))
 
 b.Loop(a.ScreenUpdate)
