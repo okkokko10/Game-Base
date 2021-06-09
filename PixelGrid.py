e5d9a146f7e9443caf19c7f0e230629b39a3b348
okkokko10 <okko.heinio@gmail.com>
2021-06-09 15:08:27 +0300

    updated inputs

--- PixelGrid.py
+++ PixelGrid.py
@@ -2,52 +2,93 @@ from Screen import *
 import numpy as np
 
 class PixelGrid(GameObject):
-    def __init__(self,size):
-        self.size=size
-        self.grid= np.ndarray(size,bool)
-        self.grid.fill(False)
-        print(self.grid)
+    def __init__(self,size,dimensions,pos):
+        self.size=iV(size)
+        self.grid= np.ndarray(size,int)
+        self.grid.fill(0)
+        self.pos=pos
+        self.dim=dimensions
+        self.surface=None
         pass
     def Remove(self):
         pass
     def Init(self,scene):
         pass
     def Draw(self,canvas:ScaledCanvas):
-        surf=pygame.Surface(self.size)
-        surf.lock()
-        for x in range(self.size[0]):
-            for y in range(self.size[1]):
-                surf.set_at((x,y),(0,0,100*self.getPoint((x,y))))
-        surf.unlock()
-        canvas.Blit(surf,V(-3,-3),V(5,5))
+        if self.surface:
+            canvas.Blit(self.surface,self.pos,(self.dim))
     def Update(self,scene):
         pass
-    def getPoint(self,pos):
-        a= self.grid[pos[0]%self.size[0],pos[1]%self.size[1]]
-        return a
-    def setPoint(self,pos,value):
-        self.grid[pos[0]%self.size[0],pos[1]%self.size[1]]=value
-class Conway(PixelGrid):
-    def Init(self,scene:Scene):
-        self.setPoint((5,5),1)
-        self.setPoint((5,6),1)
-        self.setPoint((4,5),1)
-        self.setPoint((6,5),1)
-    def Update(self,scene:Scene):
+    def UpdateSurface(self):
+        self.surface=pygame.Surface(self.size)
+        self.surface.lock()
         for x in range(self.size[0]):
             for y in range(self.size[1]):
-                s=0
-                for p in (1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1):
-                    if self.getPoint((x+p[0],y+p[1]))==True:
-                        s+=1
-                if s==3:
-                    self.setPoint((x,y),True)
-                elif s!=2:
-                    self.setPoint((x,y),False)
+                self.surface.set_at((x,y),(0,0,100*self.getPoint((x,y))))
+        self.surface.unlock()
+        
+    def getPoint(self,pos):
+        x,y = self.WarpInside(pos)
+        a= self.grid[x,y]
+        return a
+    def setPoint(self,pos,value):
+        x,y = self.WarpInside(pos)
+        self.grid[x,y]=value
+    def TransformPos(self,pos):
+        x,y=(pos-self.pos)
+        return V(x/self.dim.x*self.size[0],y/self.dim.y*self.size[1])
+    def IsInside(self,pos):
+        return (0<=pos[0]<self.size[0]) and (0<=pos[0]<self.size[0])
+    def WarpInside(self,pos):
+        return pos[0]%self.size[0],pos[1]%self.size[1]
+    def FillGrid(self,value):
+        self.grid.fill(value)
+class Conway(PixelGrid):
+    def Init(self,scene:Scene):
+        self.run=True
+    def Update(self,scene:Scene):
+        u=False
+        oldGrid =self.grid.copy()
+        if self.run:
+            u=True
+            for x in range(self.size[0]):
+                for y in range(self.size[1]):
+                    if True:
+                        s=0
+                        for p in (1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1):
+                            ax,ay=self.WarpInside((x+p[0],y+p[1]))
+                            if oldGrid[ax,ay]==True:
+                                s+=1
+                        if s==3:
+                            self.setPoint((x,y),True)
+                        elif s!=2:
+                            self.setPoint((x,y),False)
+                    else:
+                        s=0
+                        for p in (1,1),(-1,2),(0,1),(0,-1):
+                            ax,ay=self.WarpInside((x+p[0],y+p[1]))
+                            if oldGrid[ax,ay]==True:
+                                s+=1
+                        if s==1:
+                            self.setPoint((x,y),True)
+                        elif s==0:
+                            self.setPoint((x,y),False)
+        if scene.inputs.IsMousePressed(1):
+            u=True
+            pos=self.TransformPos(scene.inputs.GetMousePos())
+            if self.IsInside(pos):
+                self.setPoint(iV(pos),True)
+        if scene.inputs.IsKeyDown(pygame.K_e):
+            self.run = not self.run
+        if scene.inputs.IsKeyDown(pygame.K_w):
+            u=True
+            self.FillGrid(0)
+        if u:
+            self.UpdateSurface()
         pass
 
 a=Scene()
 b=Display(V(800,800))
-a.AddObject(Conway((10,10)))
+a.AddObject(Conway((64,64),V(8,8),V(-4,-4)))
 
 b.Loop(a.ScreenUpdate)
--- Screen.py
+++ Screen.py
@@ -2,8 +2,11 @@ import pygame
 
 def V(x,y):
     return pygame.Vector2(x,y)
+def Vi(v):
+    return pygame.Vector2(v)
 def iV(v):
-    return int(v.x),int(v.y)
+    return int(v[0]),int(v[1])
+    
 
 class Display:
     def __init__(self,size):
@@ -134,54 +137,60 @@ class Inputs:
                     self._MouseUp(e.__dict__['button'])
     def IsKeyPressed(self,key):
         if key in self.keysDown:
-            if self.keysDown[key]!=3:
+            if self.keysDown[key]&4:
                 return True
         return False
     def IsKeyDown(self,key):
         if key in self.keysDown:
-            if self.keysDown[key]==1:
+            if self.keysDown[key]&1:
                 return True
         return False
     def IsKeyUp(self,key):
         if key in self.keysDown:
-            if self.keysDown[key]==3:
+            if self.keysDown[key]&2:
                 return True
         return False
     def _KeyUp(self,key):
-        self.keysDown[key]=3
+        self.keysDown[key]|=2 #x1x
     def _KeyDown(self,key):
-        self.keysDown[key]=1
+        self.keysDown[key]=5 #101
     def _UpdateKeys(self):
+        remove=[]
         for k in self.keysDown:
-            if self.keysDown[k]==1:
-                self.keysDown[k]=2
-            elif self.keysDown[k]==3:
-                del self.keysDown[k]
+            self.keysDown[k] &= 6 #110
+            if self.keysDown[k]&2:
+                self.keysDown[k] &= 3 #011
+                remove.append(k)
+        for k in remove:
+            del self.keysDown[k]
     def IsMousePressed(self,key):
         if key in self.mouseButtonsDown:
-            if self.mouseButtonsDown[key]!=3:
+            if self.mouseButtonsDown[key]&4:
                 return True
         return False
     def IsMouseDown(self,key):
         if key in self.mouseButtonsDown:
-            if self.mouseButtonsDown[key]==1:
+            if self.mouseButtonsDown[key]&1:
                 return True
         return False
     def IsMouseUp(self,key):
         if key in self.mouseButtonsDown:
-            if self.mouseButtonsDown[key]==3:
+            if self.mouseButtonsDown[key]&2:
                 return True
         return False
     def _MouseUp(self,key):
-        self.mouseButtonsDown[key]=3
+        self.mouseButtonsDown[key]|=2
     def _MouseDown(self,key):
-        self.mouseButtonsDown[key]=1
+        self.mouseButtonsDown[key]=5
     def _UpdateMouse(self):
+        remove=[]
         for k in self.mouseButtonsDown:
-            if self.mouseButtonsDown[k]==1:
-                self.mouseButtonsDown[k]=2
-            elif self.mouseButtonsDown[k]==3:
-                del self.mouseButtonsDown[k]
+            self.mouseButtonsDown[k]&=6
+            if self.mouseButtonsDown[k]&2:
+                self.mouseButtonsDown[k] &= 3
+                remove.append(k)
+        for k in remove:
+            del self.mouseButtonsDown[k]
     def GetMousePos(self):
         return self.mousePos
     def GetTrueMousePos(self):
